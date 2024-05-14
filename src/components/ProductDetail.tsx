d8557a32a7ac0bea37e1bf4a672edb1094d6b16d
Juan Carlos <juan.constantino1202@gmail.com>
2024-05-13 21:30:10 -0600

    first commit

---
+++ src/components/ProductDetail.tsx
@@ -0,0 +1,74 @@
+import { deleteProduct } from "../services/ProductService"
+import { Product } from "../types"
+import { formatCurrency } from "../utils"
+import { ActionFunctionArgs, Form, redirect, useFetcher, useNavigate } from "react-router-dom"
+
+type ProductDetailProps = {
+    product: Product
+}
+
+export async function action({ params }: ActionFunctionArgs) {
+    if (params.id !== undefined) {
+        await deleteProduct(+params.id)
+        return redirect("/")
+    }
+
+
+}
+
+export default function ProductDetail({ product }: ProductDetailProps) {
+
+    const fetcher = useFetcher()
+    const navigate = useNavigate()
+    const isAvailable = product.availability
+    return (
+        <tr className="border-b ">
+            <td className="p-3 text-lg text-gray-800">
+                {product.name}
+            </td>
+            <td className="p-3 text-lg text-gray-800">
+                {formatCurrency(product.price)}
+            </td>
+            <td className="p-3 text-lg text-gray-800">
+                <fetcher.Form method="POST">
+                    <button
+                        type="submit"
+                        name="id"
+                        value={product.id}
+                        className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-sm 
+                        uppercase font-bold w-full border border-black hover:cursor-pointer`}
+                    >
+                        {isAvailable ? 'Disponible' : 'No disponible'}
+                    </button>
+                </fetcher.Form>
+            </td>
+            <td className="p-3 text-lg text-gray-800 ">
+                <div className="flex gap-2 items-center">
+                    <button
+                        onClick={() => navigate(`/productos/${product.id}/editar`)}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs 
+                        text-center"
+                    >Editar</button>
+
+                    <Form
+                        className="w-full"
+                        method="POST"
+                        action={`productos/${product.id}/eliminar`}
+                        onSubmit={(e) => {
+                            if (!confirm('Eliminar?')) {
+                                e.preventDefault()
+                            }
+                        }}
+                    >
+                        <input
+                            type="submit"
+                            className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs 
+                            text-center"
+                            value="Eliminar"
+                        />
+                    </Form>
+                </div>
+            </td>
+        </tr>
+    )
+}
---
+++ src/router.tsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom"
+import Layout from "./layouts/Layout"
+import Products, { action as updateAvailabilityAction, loader as productsLoader } from "./views/Products"
+import NewProduct, { action as newProductAction } from "./views/NewProduct"
+import EditProducts, { loader as editProductLoader, action as editProductAction } from "./views/EditProducts"
+import { action as deleteProductAction } from "./components/ProductDetail"
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Products />,
+                loader: productsLoader,
+                action: updateAvailabilityAction
+            },
+            {
+                path: 'productos/nuevo',
+                element: <NewProduct />,
+                action: newProductAction
+            },
+            {
+                path: 'productos/:id/editar', //ROA pattern
+                element: <EditProducts />,
+                loader: editProductLoader,
+                action: editProductAction
+            },
+            {
+                path: 'productos/:id/eliminar',
+                action: deleteProductAction
+            }
+        ]
+    }
+])
